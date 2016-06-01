6a381e8055d4436051450c8fb8dcb8b802bf676e
unifieddigitalmedia <Home@Machels-MacBook-Pro.local>
2016-06-01 20:13:55 +0100

    initial commit
    uploading files and folders

---
+++ public/scripts/normal.js
@@ -0,0 +1,28 @@
+var showModal = function(para) {
+
+var modal = document.getElementById('myModal');
+
+var modalImg = document.getElementById("img01");
+
+var captionText = document.getElementById("caption");
+
+    modal.style.display = "block";
+
+    modalImg.src = para.alt;
+    
+    modalImg.alt = para.alt;
+    
+   
+
+}
+
+
+
+
+var downLoad = function() {
+
+alert("We are sorry but our plugin is currently being reviewed by Wordpress before it will be available.We expect this process to last no longer than 6 days.Come back back then.");
+    
+   
+
+}
---
+++ server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/*
+var cl = console.log;
+console.log = function(){
+  console.trace();
+  cl.apply(console,arguments);
+};
+*/
+
+process.env.NODE_CONFIG_DIR = './config/env';
+
+// Requires meanio .
+var mean = require('meanio');
+var cluster = require('cluster');
+var deferred = require('q').defer();
+var compression = require('compression');
+
+var express = require('express');
+
+var app = express();
+
+app.use(compression());
+
+var port = process.env.PORT || 5000;
+
+app.use('/', express.static(__dirname + '/'));
+
+app.use(express.static('public'));
+
+app.use(express.static('bower_components'));
+
+app.get('/', function (req, res) {
+
+  res.sendFile(path.join(__dirname + "/index.html"));
+
+});
+
+app.listen(port, function () {
+
+  console.log('Example app listening on port 5000!');
+
+});
